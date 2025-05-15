f8bca1ab196e367459de958653632ea7a7248220
peachkalemarsexpress <118355324+peachkalemarsexpress@users.noreply.github.com>
2025-05-15 08:16:37 +0200

    Update lib.rs
    Update for use part of lib in unix, for preprocessing/validating *.o file

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 #![feature(c_variadic)]
 #![feature(core_intrinsics)]
 
+#[cfg(target_os = "windows")]
 mod beacon;
 mod parse;
 mod error;
@@ -12,5 +13,7 @@ mod beacon_pack;
 pub use beacon_pack::*;
 
 /// Module containing the code that will load the COFF
+#[cfg(target_os = "windows")]
 mod loader;
+#[cfg(target_os = "windows")]
 pub use loader::*;
